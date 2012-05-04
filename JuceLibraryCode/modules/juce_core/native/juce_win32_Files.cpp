ac649f6e7e9123a22f31cd69f690c5f6870efc56
Sonic Fiftynine <supermario30@gmail.com>
2012-05-03 22:04:30 -0500

    Added XP and Vista implementation of getUserLocale.

--- JuceLibraryCode/modules/juce_core/native/juce_win32_Files.cpp
+++ JuceLibraryCode/modules/juce_core/native/juce_win32_Files.cpp
@@ -956,3 +956,24 @@ int NamedPipe::write (const void* sourceBuffer, int numBytesToWrite, int timeOut
 
     return bytesWritten;
 }
+
+String LocalisedStrings::getUserLocale()
+{
+    String locale ("en-US");
+    //#ifdef Windows < Windows Vista
+    int bufSize = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, nullptr, 0);
+    HeapBlock<wchar_t> lcode (bufSize);
+    int result = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, lcode, bufSize);
+    String language = static_cast <const wchar_t*> (lcode);
+    bufSize = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, nullptr, 0);
+    HeapBlock<wchar_t> ccode (bufSize);
+    result = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO3166CTRYNAME, ccode, bufSize);
+    String country = static_cast <const wchar_t*> (ccode);
+    locale = language + "-" + country;
+    //#elif Windows >= Windows Vista
+    //HeapBlock<wchar_t> localeName (LOCALE_NAME_MAX_LENGTH);
+    //int defaultLocaleSuccess  = GetUserDefaultLocaleName(localeName, LOCALE_NAME_MAX_LENGTH);
+    //if (defaultLocaleSuccess) locale = static_cast <const wchar_t*> (localeName);
+    //#endif
+    return locale;
+}
